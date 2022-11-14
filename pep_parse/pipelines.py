0b28b9b574726cc7563125a487e6213b98ed91dc
n3navizhy <93191625+n3navizhy@users.noreply.github.com>
2022-11-14 22:49:00 +0300

    Ревью 2

--- pep_parse/pipelines.py
+++ pep_parse/pipelines.py
@@ -1,32 +1,28 @@
 import collections
 import csv
 import datetime
-from pathlib import Path
 
-BASE_DIR = Path(__file__).parent.parent
-FILE_DIR = 'results'
-DATE_MASK = "%Y-%m-%d_%H-%M-%S"
-PEP_FILE = FILE_DIR + '/pep_%(time)s.csv'
+from pep_parse.settings import BASE_DIR, FILE_DIR, DATE_MASK
 
 
 class PepParsePipeline:
-    pep_sum = collections.defaultdict(int)
+    def __init__(self):
+        self.result_path = BASE_DIR / FILE_DIR
+        self.result_path.mkdir(exist_ok=True)
+        self.pep_sum = {}
 
     def open_spider(self, spider):
-        pass
+        self.pep_sum = collections.defaultdict(int)
 
     def process_item(self, item, spider):
         self.pep_sum[item['status']] += 1
         return item
 
     def close_spider(self, spider):
-        result_path = BASE_DIR / FILE_DIR
-        result_path.mkdir(exist_ok=True)
         now_time = datetime.datetime.now().strftime(DATE_MASK)
-        file_path = result_path / f'status_summary_{now_time}.csv'
+        file_path = self.result_path / f'status_summary_{now_time}.csv'
         with open(file_path, mode='w', encoding='utf-8') as f:
-            csv_writer = csv.writer(f, dialect=csv.unix_dialect)
-            csv_writer.writerows([
+            csv.writer(f, dialect=csv.unix_dialect).csv_writer.writerows([
                 [('Статус'), ('Количество')],
                 *self.pep_sum.items(),
                 ['Total', sum(self.pep_sum.values())]
--- pep_parse/settings.py
+++ pep_parse/settings.py
@@ -1,4 +1,9 @@
-from pep_parse.pipelines import PEP_FILE
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent.parent
+FILE_DIR = 'results'
+PEP_FILE = FILE_DIR + '/pep_%(time)s.csv'
+DATE_MASK = "%Y-%m-%d_%H-%M-%S"
 BOT_NAME = 'pep_parse'
 
 SPIDER_MODULES = ['pep_parse.spiders']
